0e404dd5884d596f88a84dcad96d4e1d85f50ba0
Harrison Gerber <gerberhe@s.dcsdk12.org>
2016-03-11 09:49:55 -0700

    Finished activity 6

--- src/PictureTester.java
+++ src/PictureTester.java
@@ -26,6 +26,13 @@ public class PictureTester
     caterpillar.explore();
   }
   
+  public static void testMirrorVerticalRightToLeft() {
+	  Picture caterpillar = new Picture("caterpillar.jpg");
+	  caterpillar.explore();
+	  caterpillar.mirrorVerticalRightToLeft();
+	  caterpillar.explore();
+  }
+  
   /** Method to test mirrorTemple */
   public static void testMirrorTemple()
   {
@@ -92,8 +99,9 @@ public class PictureTester
     //testKeepOnlyGreen();
 //    testNegate();
 //    testGrayscale();
-    testFixUnderwater();
-    //testMirrorVertical();
+//    testFixUnderwater();
+//    testMirrorVertical();
+	testMirrorVerticalRightToLeft();
     //testMirrorTemple();
     //testMirrorArms();
     //testMirrorGull();
